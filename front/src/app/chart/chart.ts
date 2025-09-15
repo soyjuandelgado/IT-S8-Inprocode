17bab56f1925a8c958d1d709872cc68ab9d85214
Juan Delgado <soy.juan.delgado@gmail.com>
2025-09-15 20:58:00 +0200

    added example chart

--- front/src/app/chart/chart.ts
+++ front/src/app/chart/chart.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-chart',
-  imports: [],
+  imports: [ChartModule],
   templateUrl: './chart.html',
   styleUrl: './chart.scss'
 })
 export class Chart {
-
+  basicData = {
+      labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+      datasets: [
+        {
+          label: 'Ventas 2024',
+          backgroundColor: '#42A5F5',
+          data: [65, 59, 80, 81]
+        }
+      ]
+    };
+  options = {
+      plugins: {
+        legend: {
+          labels: {
+            color: '#495057'
+          }
+        },
+        title: {
+          display: true,
+          text: 'Ventas Trimestrales',
+          color: '#333'
+        }
+      },
+      scales: {
+        x: {
+          ticks: { color: '#495057' },
+          grid: { color: '#ebedef' }
+        },
+        y: {
+          ticks: { color: '#495057' },
+          grid: { color: '#ebedef' }
+        }
+      }
+    };
 }
