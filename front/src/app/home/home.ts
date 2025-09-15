9a4b19ef09bd74236d56356f3bbb6551eb2b3cae
Juan Delgado <soy.juan.delgado@gmail.com>
2025-09-15 20:33:25 +0200

    feat: home page done

--- front/src/app/home/home.ts
+++ front/src/app/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CardModule, ButtonModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
