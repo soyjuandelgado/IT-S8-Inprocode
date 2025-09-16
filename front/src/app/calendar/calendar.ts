142357bd3e9778413dbc02bebcf101c1a7f7e8ae
Juan Delgado <soy.juan.delgado@gmail.com>
2025-09-16 10:37:54 +0200

    added FullCalendar

--- front/src/app/calendar/calendar.ts
+++ front/src/app/calendar/calendar.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
+  imports: [FullCalendarModule],
   templateUrl: './calendar.html',
-  styleUrl: './calendar.scss'
+  styleUrl: './calendar.scss',
 })
 export class Calendar {
-
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin, interactionPlugin],
+    weekends: true, // mostrar fines de semana
+    events: [
+      { title: 'Evento 1', date: '2025-09-17' },
+      { title: 'Evento 2', date: '2025-09-18' },
+    ],
+  };
 }
